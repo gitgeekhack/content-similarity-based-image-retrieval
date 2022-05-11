df6d1734ee42bc2e59f77799edffaea6d727c5ba
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-05-11 10:39:04 +0530

    code changes

--- app/database/connection_manager.py
+++ app/database/connection_manager.py
@@ -1,4 +1,3 @@
-# imporing required libraries
 from elasticsearch import Elasticsearch
 import os
 from app.common.utils import read_properties_file, MonoState, load_config
@@ -8,15 +7,14 @@ from app.config import CONFIG
 
 # class for database connection activities
 class DatabaseConnection(MonoState):
-    # function for connecting to database
+    
     def connect(self):
         config = read_properties_file(os.path.join(APP_ROOT, "environment.properties"))
         config_name = os.getenv('APPLICATION_ENVIRONMENT', config['environment'])
-        config_environment = load_config(CONFIG[config_name])  # importing environment specific properties
+        config_environment = load_config(CONFIG[config_name])
         es = Elasticsearch(config_environment.HOST,
                            basic_auth=(config_environment.USERNAME, config_environment.PASSWORD), verify_certs=False)
         return es
 
-    # function for closing database connection
     def close(self, es):
         es.close()
--- app/resource/image_similarity/imagesimilarity.py
+++ app/resource/image_similarity/imagesimilarity.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 from flask import flash, request, redirect, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 from app.common.utils import allowed_file
-from app.constant import UPLOAD_FOLDER
+from app.constant import UPLOAD_FOLDER, MAXIMUM_UPLOAD
 from app.service.search_and_index_images import ImageIndexer, ImageSearcher
 
 flask_main_app = Blueprint('main_app', __name__)  # creating Blueprint for flask app
@@ -35,8 +35,8 @@ def index_images():
         return redirect(request.url)
     files = request.files.getlist('files[]')
 
-    if len(files) > 100:
-        flash('Maximum 100 files allowed at a time', 'warning')  # only 100 files allowed at a time
+    if len(files) > MAXIMUM_UPLOAD:
+        flash(f'Maximum {MAXIMUM_UPLOAD} files allowed at a time', 'warning')  # only 100 files allowed at a time
         return redirect(request.url)
 
     allowed_files = []  # allowed file list
